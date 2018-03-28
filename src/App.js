c2348eb347cf71ca3abeb760cc9a364c6793eeee
Dylan Pinn <dylan@dylanpinn.com>
2018-03-29 00:19:55 +1100

    Fix bug

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React<*> {
+class App extends React.Component<*> {
   render() {
     return (
       <div className="App">
