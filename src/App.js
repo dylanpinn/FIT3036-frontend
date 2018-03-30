acedde375cf1fead90c7f0f15dddf4c401a4e79d
Dylan Pinn <dylan@dylanpinn.com>
2018-03-30 18:49:39 +1100

    Remove logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // @flow
 import * as React from 'react';
 import Map from './Map';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component<*> {
@@ -9,11 +8,9 @@ class App extends React.Component<*> {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <Map
-          isMarkerShown
           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `400px` }} />}
