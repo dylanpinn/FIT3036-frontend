bb1ae6192bbb2b1e9d7c0ed1f0a9aed457d0de31
Dylan Pinn <dylan@dylanpinn.com>
2018-03-30 19:37:04 +1100

    Add Rectangle to Map

--- src/Map.js
+++ src/Map.js
@@ -4,19 +4,32 @@ import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
-  Marker
+  Marker,
+  Rectangle
 } from 'react-google-maps';
 
 type Props = {
   isMarkerShown?: boolean
 };
 
+const defaultCenter = {
+  lat: -37.911716,
+  lng: 145.127197
+};
+
 const Map = withScriptjs(
   withGoogleMap((props: Props) => (
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-      {props.isMarkerShown && (
-        <Marker position={{ lat: -34.397, lng: 150.644 }} />
-      )}
+    <GoogleMap defaultZoom={11} defaultCenter={defaultCenter}>
+      {props.isMarkerShown && <Marker position={defaultCenter} />}
+      <Rectangle
+        draggable={true}
+        bounds={{
+          north: -37.9,
+          south: -37.8,
+          east: 145.1,
+          west: 145.0
+        }}
+      />
     </GoogleMap>
   ))
 );
