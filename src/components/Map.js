41ca7b3da1bd0253d7aa7905b2d148c170e8a746
Dylan Pinn <dylan@dylanpinn.com>
2018-05-19 00:38:23 +1000

    Improve Map Square (#22)
    * Add Rectangle in center that is 1km
    
    * Fix flow and eslint errors
    
    * Hide Circle
    
    * Improve type
    
    * Add smoke test for Map
    
    * Travis - Run flow
    
    * Exclude tests from flow coverage report

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,39 +4,117 @@ import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
-  Rectangle
+  Rectangle,
+  Circle
+} from 'react-google-maps';
+import type { Coordinate, Rectangle as RectangleType } from '../types';
+import type {
+  Rectangle as RectangleComponent,
+  GoogleMap as MapComponent,
+  Circle as CircleComponent
 } from 'react-google-maps';
 
-type Coordinate = {
-  lat: number,
-  lng: number
-};
-
 type Props = {
-  rectangle?: boolean,
-  rectangleOnDragEnd: (e: MouseEvent) => void,
-  rectangleMounted: (ref: any) => void,
-  centre: Coordinate
+  rectangle: ?RectangleType,
+  center: Coordinate,
+  updateCenter: (c: Coordinate) => void,
+  updateRect: (r: RectangleType) => void
 };
+const GOOGLE_MAPS_API_KEY = 'AIzaSyC60FyPR7iVZWTMOjoWJdKrnRsM4MbTsUY';
 
-const initialRectangle = {
-  north: -37.9,
-  south: -37.8,
-  east: 145.1,
-  west: 145.0
-};
+class Map extends React.Component<Props> {
+  rectangle: ?RectangleComponent;
+  map: ?MapComponent;
+  circle: ?CircleComponent;
+  timer: ?TimeoutID;
 
-const Map = withScriptjs(
-  withGoogleMap((props: Props) => (
-    <GoogleMap defaultZoom={11} center={props.centre}>
-      {props.rectangle && (
-        <Rectangle
-          draggable={true}
-          bounds={initialRectangle}
-          onDragEnd={props.rectangleOnDragEnd}
-          ref={rectangle => props.rectangleMounted(rectangle)}
+  rectangleMounted = (ref: RectangleComponent) => {
+    this.rectangle = ref;
+  };
+
+  mapMounted = (ref: MapComponent) => {
+    this.map = ref;
+  };
+
+  circleMounted = (ref: MapComponent) => {
+    this.circle = ref;
+  };
+
+  onDragEnd = () => {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+    let _this = this;
+    this.timer = setTimeout(function() {
+      if (_this.map) {
+        const bounds = _this.map.getCenter();
+        const lat = bounds.lat();
+        const lng = bounds.lng();
+        _this.props.updateCenter({ lat, lng });
+        if (_this.circle) {
+          const bounds = _this.circle.getBounds();
+          const rectangle = {
+            south: bounds.f.f,
+            north: bounds.f.b,
+            east: bounds.b.f,
+            west: bounds.b.b
+          };
+          _this.props.updateRect(rectangle);
+        }
+      }
+    }, 500);
+  };
+
+  render() {
+    const mapURL = `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${GOOGLE_MAPS_API_KEY}&libraries=geometry,drawing,places`;
+    return (
+      <div>
+        <RenderMap
+          googleMapURL={mapURL}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `500px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          rectangleMounted={this.rectangleMounted}
+          circleMounted={this.circleMounted}
+          onMapChange={this.onDragEnd}
+          mapMounted={this.mapMounted}
+          center={this.props.center}
+          rectangle={this.props.rectangle}
         />
-      )}
+      </div>
+    );
+  }
+}
+
+type RenderMapProps = {
+  center: Coordinate,
+  rectangle: ?RectangleType,
+  onMapChange: () => void,
+  mapMounted: (r: MapComponent) => void,
+  rectangleMounted: (r: RectangleComponent) => void,
+  circleMounted: (r: CircleComponent) => void
+};
+
+const RenderMap = withScriptjs(
+  withGoogleMap((props: RenderMapProps) => (
+    <GoogleMap
+      defaultZoom={14}
+      center={props.center}
+      onBoundsChanged={props.onMapChange}
+      ref={map => props.mapMounted(map)}
+      {...props}
+    >
+      <Rectangle
+        draggable={false}
+        bounds={props.rectangle}
+        ref={rectangle => props.rectangleMounted(rectangle)}
+      />
+      <Circle
+        visible={false}
+        center={props.center}
+        radius={1000}
+        ref={circle => props.circleMounted(circle)}
+      />
     </GoogleMap>
   ))
 );
--- src/types/index.js
+++ src/types/index.js
@@ -6,3 +6,8 @@ export type Rectangle = {
   east: number,
   west: number
 };
+
+export type Coordinate = {
+  lat: number,
+  lng: number
+};
